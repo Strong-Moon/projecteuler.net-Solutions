765bae3b931c7778fd4d482f41953710ce8f5985
Berkay Pehlivan <110940123+Strong-Moon@users.noreply.github.com>
2022-08-17 17:25:12 +0300

    uploading question 2

---
+++ src/Problem-2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1 = 1;
+            long num2 = 1;
+            long result = 0;
+            long sum = 0;
+
+            while (result < 4000000)
+            {
+                if ((result % 2) == 0)
+                {
+                    sum += result;
+                }
+
+                result = num1 + num2;
+                num2 = num1;
+                num1 = result;
+            }
+            System.Console.WriteLine($"The sum of the even-values is {sum}");
+        }
+    }
+}

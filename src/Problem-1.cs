8272269a6488e71f0bfb49b9a81a309b823f1e8a
Berkay Pehlivan <110940123+Strong-Moon@users.noreply.github.com>
2022-08-17 16:26:28 +0300

    Add files via upload

---
+++ src/Problem-1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    sum += i;
+                } 
+            }
+            System.Console.WriteLine($"The sum of all the multiples of 3 or 5 below 1000 is {sum}.",sum);
+        }
+    }
+}
+// The answer is 233168

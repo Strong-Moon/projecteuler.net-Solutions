9b9275e4b517b6e562db7f3bd357d5588f45a7a9
Berkay Pehlivan <110940123+Strong-Moon@users.noreply.github.com>
2022-08-18 17:40:46 +0300

    Add files via upload

---
+++ src/Problem-3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    internal class Programs
+    {
+        static void Main(string[] args)
+        {
+            const long expected_number = 600851475143;
+            long num = expected_number;
+            long LF  = 0;
+
+            int counter = 2;
+            while (counter * counter <= num)
+            {
+                if (num % counter == 0)
+                {
+                    num = num / counter;
+                    LF = counter;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            if (num > LF )
+            { 
+                LF  = num;
+            }
+            System.Console.WriteLine($"The largest prime factor of the number {expected_number} is {LF}");
+        }
+    }
+}
